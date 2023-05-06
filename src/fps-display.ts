b6ffa1422df19cc8cacb4ad1538c6698040df6e9
Sam Avanesov <sam.avanesoff@gmail.com>
2023-05-06 18:45:40 -0400

    Improved alignment and used custom fonts for FPS

--- src/fps-display.ts
+++ src/fps-display.ts
@@ -8,17 +8,18 @@ const timing = {
 };
 
 const style = {
-  font: "15px Arial",
   x: 0,
   y: 0,
   height: 24,
   fps: {
-    x: 1,
-    y: 15,
+    font: "15px Arial",
+    x: 3,
+    y: 16,
   },
   averageFPS: {
-    x: 50,
-    y: 15,
+    font: "12px Arial",
+    x: 38,
+    y: 16,
   },
 };
 
@@ -29,14 +30,16 @@ export function renderFPS(canvas: HTMLCanvasElement, state: State) {
 
   timing.last = state.now;
   canvas.ctx.clearRect(style.x, style.y, canvas.width, style.height);
-  canvas.ctx.font = style.font;
+
   // draw FPS
+  canvas.ctx.font = style.fps.font;
   canvas.ctx.fillStyle = getFPSColor(state.fps);
   canvas.ctx.fillText(`${state.fps.toFixed(1)}`, style.fps.x, style.fps.y);
   // draw average FPS
+  canvas.ctx.font = style.averageFPS.font;
   canvas.ctx.fillStyle = getFPSColor(state.averageFPS);
   canvas.ctx.fillText(
-    `avg: ${state.averageFPS.toFixed(1)}`,
+    `(avg: ${state.averageFPS.toFixed(1)})`,
     style.averageFPS.x,
     style.averageFPS.y
   );
